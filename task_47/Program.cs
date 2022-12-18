c719b64d19854a3813341c355c863376ed1f45ce
Daria <macdaria1999@gmail.com>
2022-12-18 14:49:44 +0300

    Add task 47

---
+++ task_47/Program.cs
@@ -0,0 +1,21 @@
+﻿Random x = new Random();
+void InputMatrix(double[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(x.Next(-100,101) /10.0);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] coord = Console.ReadLine().Split(" ");
+int row = int.Parse(coord[0]);
+int column = int.Parse(coord[1]);
+double[,] matrix = new double[row, column];
+InputMatrix(matrix);
